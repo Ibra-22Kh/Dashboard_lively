422e448d575e46ee7280b28fda06a0c242fbbcc8
ibra-kdbra <sonarfarouq@gmail.com>
2023-03-27 23:33:21 +0300

    ending the project with beautiful charts and customizing it with very elegant look

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -308,22 +308,6 @@ const Ecommerce = () => {
               src={product9}
               alt=""
             />
-            <div className="mt-8">
-              <p className="font-semibold text-lg">React 18 coming soon!</p>
-              <p className="text-gray-400 ">By Johnathan Doe</p>
-              <p className="mt-8 text-sm text-gray-400">
-                This will be the small description for the news you have shown
-                here. There could be some great info.
-              </p>
-              <div className="mt-3">
-                <Button
-                  color="white"
-                  bgColor={currentColor}
-                  text="Read More"
-                  borderRadius="10px"
-                />
-              </div>
-            </div>
           </div>
         </div>
       </div>
