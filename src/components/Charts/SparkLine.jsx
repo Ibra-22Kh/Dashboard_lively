635d6863f90935c1fd08c09333a8d1759e94ce39
ibra-kdbra <sonarfarouq@gmail.com>
2023-03-27 23:36:16 +0300

    changed the SparkedLine to class component after reading syncfusino documents

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,8 +1,11 @@
+
 import React from 'react';
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine =({ id, height, width, color, data, type, currentColor })=>{
-  
+class SparkLine extends React.PureComponent {
+  render() {
+    const { id, height, width, color, data, type, currentColor } = this.props;
+
     return (
       <SparklineComponent
         id={id}
@@ -30,5 +33,6 @@ const SparkLine =({ id, height, width, color, data, type, currentColor })=>{
       </SparklineComponent>
     );
   }
+}
 
 export default SparkLine;
